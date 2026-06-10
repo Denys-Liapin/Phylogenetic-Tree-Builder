c60baf504ca36984f15dd015aec4241c86b2777a
Denys Liapin <denys.liapin@gmail.com>
2026-06-10 07:26:40 +0300

    Add files via upload

--- PhylogeneticTreeBuilder/src/com/bioinformatics/analyzer/IO/FastaParser.java
+++ PhylogeneticTreeBuilder/src/com/bioinformatics/analyzer/IO/FastaParser.java
@@ -1,4 +1,32 @@
 package com.bioinformatics.analyzer.IO;
+import com.bioinformatics.analyzer.model.FastaRecord;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FastaParser {
+
+    public List<FastaRecord> parse(String inputFileName) throws IOException {
+        List<FastaRecord> records = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
+            String line;
+            StringBuilder thisDNA = new StringBuilder();
+            String currentHeader = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith(">")) {
+                    if (currentHeader != null) {
+                        records.add(new FastaRecord(currentHeader, thisDNA.toString()));
+                        thisDNA.setLength(0);
+                    }
+                    currentHeader = line;
+                } else {
+                    thisDNA.append(line);
+                }
+            }
+            if (currentHeader != null) {
+                records.add(new FastaRecord(currentHeader, thisDNA.toString()));
+            }
+        }
+        return records;
+    }
 }
