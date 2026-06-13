0950b48913c4ea17dee09ded40f2367f5347a2aa
Denys Liapin <denys.liapin@gmail.com>
2026-06-13 14:50:34 +0300

    Add ProfileConverter service

---
+++ PhylogeneticTreeBuilder/src/com/bioinformatics/analyzer/alignment/ProfileConverter.java
@@ -0,0 +1,23 @@
+package com.bioinformatics.analyzer.alignment;
+import com.bioinformatics.analyzer.model.KmerPercentProfile;
+import com.bioinformatics.analyzer.model.KmerProfile;
+import java.util.HashMap;
+
+public class ProfileConverter {
+
+    public KmerPercentProfile Converter(KmerProfile rawProfile){
+        double total = 0.0;
+        HashMap<String, Integer> rawMap = rawProfile.getCodonAndQuantity();
+        HashMap<String, Double> percentMap = new HashMap<>();
+
+        for (String key : rawMap.keySet()){
+            total = total + rawMap.get(key);
+        }
+
+        for (String key : rawMap.keySet()){
+            percentMap.put(key, rawMap.get(key) / total * 100);
+        }
+
+        return new KmerPercentProfile(percentMap);
+    }
+}
