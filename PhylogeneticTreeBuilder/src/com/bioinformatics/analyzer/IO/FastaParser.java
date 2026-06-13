d0a7a3974cc23a0b00fa75098755461141d0250b
Denys Liapin <denys.liapin@gmail.com>
2026-06-13 20:12:00 +0300

    FIX: FastaParser
    The FastaRecord saving system has been reworked

--- PhylogeneticTreeBuilder/src/com/bioinformatics/analyzer/IO/FastaParser.java
+++ PhylogeneticTreeBuilder/src/com/bioinformatics/analyzer/IO/FastaParser.java
@@ -1,28 +1,34 @@
 package com.bioinformatics.analyzer.IO;
+import com.bioinformatics.analyzer.model.FastaRecord;
+
 import java.io.*;
 import java.util.ArrayList;
 
 public class FastaParser {
     private String inputFileName = "file.txt";
-    ArrayList<String> DNA_header = new ArrayList<>();
-    ArrayList<String> DNA_sequence = new ArrayList<>();
 
-    public void parse(String inputFileName) {
+    public ArrayList<FastaRecord> parse(String inputFileName) {
+        ArrayList<FastaRecord> records = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
             String line;
             StringBuilder ThisDNA = new StringBuilder();
+            String thisHeader = null;
             int i = 0;
             while ((line = reader.readLine()) != null) {
                 if (line.startsWith(">")) {
-                    DNA_header.add(line);
+                    if (thisHeader != null) {
+                        records.add(new FastaRecord(thisHeader, ThisDNA.toString()));
+                        ThisDNA.setLength(0);
+                    }
+                    thisHeader = line;
                     i++;
                 } else {
                     ThisDNA.append(line);
-                    DNA_sequence.add(i, ThisDNA);
                 }
             }
-
+            records.add(new FastaRecord(thisHeader, ThisDNA.toString()));
         }
+        return records;
     }
 
 }
